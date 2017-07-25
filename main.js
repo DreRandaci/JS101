462d87b83ee62d445f5f762460edf13f3c80e373
Dre mf Randaci <drerandaci@gmail.com>
2017-07-24 20:02:42 -0500

    added some cool shit to my web files

---
+++ main.js
@@ -0,0 +1,27 @@
+// operator: used to manipulate or change the values of your program
+
+
+
+// mathematical operators
+let sumExample = 1 + 1;
+let subExample = 1 - 1;
+let multExample = 1 * 1;
+let divideExample = 1 / 1;
+let modulusExample = 2 % 1;
+
+// == // values are equal (returns a boolean value)
+// console.log("4" == 4);
+
+// === // strict equality (value and type)
+// != // values are not equal (just for values)
+// !== // values are strictly not equal (values and types)
+
+// relational operators
+// > < >= <=
+
+// Logical operators
+
+// && (and)
+// || (or)
+// ! (not)
+
